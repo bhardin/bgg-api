d32b249f011709a277b4289d7aff595a27364146
Jeremiah Lee <jdl@Jeremiahs-MacBook-Pro.local>
2012-02-18 11:51:25 -0800

    Proof of concept for doing the API calls on demand. It just prints out the results right now. Next steps: process the resulting XML and return a nested datastructure containing the data.

--- lib/bgg_api.rb
+++ lib/bgg_api.rb
@@ -1,60 +1,36 @@
 require 'httparty'
 require 'rexml/document'
-load    'search_result.rb'
 
 # http://boardgamegeek.com/wiki/page/BGG_XML_API2#
 
 class BggApi
   include HTTParty
 
-    #base_uri "rubygems.org/api/v1"
-  base_uri "www.boardgamegeek.com/xmlapi2"
+  @@base_uri = "http://www.boardgamegeek.com/xmlapi2"
 
-  def self.thing(id)
-    response = get("/thing", :query => {:id => id})
-
-    if response.success?
-      response["items"]["item"]
-    end
-
-  end
-
-    # Find a particular game, based on its name
-  def self.search(query, options = Hash.new)
-    search_options = {:query => query}
-    
-    if options.size > 0
-      search_options.merge!(options)
-    end
-    
-    puts search_options
-    response = get("/search/", :query => search_options)
-
-    item_list = Array.new
-    
-    if response.code == 200
-      xml_data = response.body
+  protected
+  
+    def call(method, params = {})
+      response = HTTParty.get(@@base_uri + '/' + method.to_s + '/', :query => params)
       
-      doc = REXML::Document.new(xml_data)
-      
-      doc.elements.each("/items/item") do |item|
-        result = SearchResult.new(item.attributes["id"],
-                                  item.get_elements("name")[0].attributes["value"],
-                                  item.get_elements("yearpublished")[0].attributes["value"],
-                                  item.attributes["type"])
-        
-        item_list << result
+      if response.code == 200
+        xml_data = response.body
+        puts xml_data
+      else
+        raise response.code
       end
-    else
-      raise response.code
     end
     
-    item_list
-  end
+    def method_missing(method, *args)
+      puts "Method name: #{method}"
+      call(method, *args)
+    end
+  
+    def respond_to?(method)
+        true
+    end
+
 end
 
-element_list = BggApi.search("Burgund", {:type => 'boardgame'})
-
-element_list.each do |item|
-  item.print
-end
+bgg = BggApi.new
+bgg.search({:query => "Burgund", :type => 'boardgame'})
