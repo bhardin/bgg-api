6d23e97320933690a71bc07adcce6cdf9e7147f7
Jeremiah Lee <jeremiah.lee@gmail.com>
2012-08-18 12:27:18 -0700

    Adding mocked tests for the module in preparation for releasing a gem.

--- Gemfile
+++ Gemfile
@@ -9,7 +9,9 @@ gem 'xml-simple'
 # Include everything needed to run rake, tests, features, etc.
 group :development do
   gem "shoulda", ">= 0"
-  gem "bundler", "~> 1.0.0"
-  gem "jeweler", "~> 1.6.4"
+  gem "bundler", ">= 1.0.0"
+  gem "jeweler", ">= 1.6.4"
   gem "rcov", ">= 0"
+  gem 'rspec'
+  gem 'webmock'
 end
--- spec/bgg_api_spec.rb
+++ spec/bgg_api_spec.rb
@@ -1,12 +1,63 @@
-require "rspec"
+require 'rspec'
+require 'bgg_api'
+require 'webmock/rspec'
 
+describe 'BGG Search' do
 
-describe "BGG Search" do
+  it 'some results come back' do
+    stub_request(:any, "http://www.boardgamegeek.com/xmlapi2/search").with(:query => {:query => 'Burgund', :type => 'boardgame'}).to_return(:body => File.new('sample_data/search?query=Burgund&type=boardgame'), :status => 200)
 
-  it "some results come back" do
     bgg = BggApi.new
-    results = bgg.search({:query => "Burgund", :type => 'boardgame'})
+    results = bgg.search({:query => 'Burgund', :type => 'boardgame'})
 
     results.should_not be_nil
+  end
+end
+
+describe 'BGG Thing' do
+  it 'searches for and returns a thing' do
+    stub_request(:any, "http://www.boardgamegeek.com/xmlapi2/thing").with(:query => {:id => '84876', :type => 'boardgame'}).to_return(:body => File.new('sample_data/thing?id=84876&type=boardgame'), :status => 200)
+
+    bgg = BggApi.new
+    results = bgg.thing({:id => '84876', :type => 'boardgame'})
+
+    results.should_not be_nil
+    results['item'][0]['id'].should == '84876'
+  end
+end
+
+describe 'BGG Collection' do
+  it 'retrieves a collection' do
+    stub_request(:any, "http://www.boardgamegeek.com/xmlapi2/collection").with(:query => {:own => '1', :name => 'texasjdl', :type => 'boardgame'}).to_return(:body => File.new('sample_data/collection?username=texasjdl&own=1&excludesubtype=boardgameexpansion'), :status => 200)
+
+    bgg = BggApi.new
+    results = bgg.collection({:name => 'texasjdl', :own => '1', :type => 'boardgame'})
+
+    results.should_not be_nil
+    results['item'][0]['objectid'].should == '421'
+  end
+end
+
+describe 'BGG Hot Items' do
+  it 'retrieves the current hot boardgames' do
+    stub_request(:any, "http://www.boardgamegeek.com/xmlapi2/hot").with(:query => {:type => 'boardgame'}).to_return(:body => File.new('sample_data/hot?type=boardgame'), :status => 200)
+
+    bgg = BggApi.new
+    results = bgg.hot({:type => 'boardgame'})
+
+    results.should_not be_nil
+    results['item'][0]['rank'].should == '1'
+  end
+end
+
+describe 'BGG Plays' do
+  it 'retrieves the plays for a user' do
+    stub_request(:any, "http://www.boardgamegeek.com/xmlapi2/plays").with(:query => {:id => '84876', :username => 'texasjdl'}).to_return(:body => File.new('sample_data/plays?username=texasjdl&id=84876'), :status => 200)
+
+    bgg = BggApi.new
+    results = bgg.plays({:id => '84876', :username => 'texasjdl'})
+
+    results.should_not be_nil
+    results['total'].should == '27'
   end
 end
