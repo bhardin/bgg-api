c7ad1a808d11c1444065fcf7d7bcd2401c5bf42d
Marcello Missiroli <prof.missiroli@gmail.com>
2013-01-18 12:05:42 +0100

    * Added specialized method(search  by id)

--- lib/bgg_api.rb
+++ lib/bgg_api.rb
@@ -19,20 +19,38 @@ class BggApi
 
     response = HTTParty.get(@@base_uri + '/search', :query=> {:query => name, :type => type})
 
-    #puts response.body
-
     return if response.code != 200
-
 
     result = Array.new
     xml = XmlSimple.xml_in(response.body)
-    puts xml
     return if xml["total"]=="0"
 
     xml["item"].each do  |item|
-      result << Hash[:name, item["name"][0]["value"], :name_type,item["name"][0]["value"], :type, item["type"],  :id , item["id"].to_i]
+      result << Hash[:name, item["name"][0]["value"], :type, item["type"],  :id , item["id"].to_i]
     end
     return result
+  end
+
+  def self.search_boardgame_by_id(id,type='boardgame')
+    response = HTTParty.get(@@old_uri + '/boardgame/'+id.to_s)
+    return if response.code != 200
+
+    xml = XmlSimple.xml_in(response.body)
+    @alternate_names = Array.new
+    xml["boardgame"][0]["name"].each_with_index do |name,index|
+      if name["primary"]=="true"
+        @primary_name=name["content"]
+        primary_index = index
+      else
+        @alternate_names <<name["content"]
+      end
+      xml["boardgame"][0]["name"].slice!(primary_index) unless primary_index.nil?
+    end
+    return Hash[:id, id, :name, @primary_name,:minplayers,xml["boardgame"][0]["minplayers"][0],:maxplayers,xml["boardgame"][0]["maxplayers"][0],
+                :age,xml["boardgame"][0]["age"][0], :description, xml["boardgame"][0]["description"][0],:playingtime,xml["boardgame"][0]["playingtime"][0],
+                :thumbnail,xml["boardgame"][0]["thumbnail"][0], :image ,xml["boardgame"][0]["image"][0], :alternatenames,  @alternate_names.sort,
+                :yearpublished,xml["boardgame"][0]["yearpublished"][0] ]
+
   end
 
 
