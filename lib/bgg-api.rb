701663e95a82f836744269730e7eb07f97c605ca
Ryan MacGillivray <rmacg89@gmail.com>
2014-02-10 19:30:40 +0000

    refactor to use hashbuilder class

--- lib/bgg-api.rb
+++ lib/bgg-api.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'xmlsimple'
+require 'hash_builder'
 
 # http://boardgamegeek.com/wiki/page/BGG_XML_API2#
 
@@ -15,6 +16,8 @@ class BggApi
   @@base_uri = "http://www.boardgamegeek.com/xmlapi2"
   @@old_uri = "http://www.boardgamegeek.com/xmlapi"
 
+  @hash = HashBuilder
+
   def self.search_by_name(name,type='boardgame')
 
     response = HTTParty.get(@@base_uri + '/search', :query=> {:query => name, :type => type})
@@ -26,7 +29,7 @@ class BggApi
     return if xml["total"]=="0"
 
     xml["item"].each do  |item|
-      result << Hash[:name, item["name"][0]["value"], :type, item["type"],  :id , item["id"].to_i]
+      @hash.build_basic_game_results(item, result)
     end
     return result
   end
@@ -46,11 +49,8 @@ class BggApi
       end
       xml["boardgame"][0]["name"].slice!(primary_index) unless primary_index.nil?
     end
-    return Hash[:id, id, :name, @primary_name,:minplayers,xml["boardgame"][0]["minplayers"][0],:maxplayers,xml["boardgame"][0]["maxplayers"][0],
-                :age,xml["boardgame"][0]["age"][0], :description, xml["boardgame"][0]["description"][0],:playingtime,xml["boardgame"][0]["playingtime"][0],
-                :thumbnail,xml["boardgame"][0]["thumbnail"][0], :image ,xml["boardgame"][0]["image"][0], :alternatenames,  @alternate_names.sort,
-                :yearpublished,xml["boardgame"][0]["yearpublished"][0] ]
-
+    result = @hash.build_complex_boardgame_result(@primary_name, @alternate_names, xml, id)
+    return result
   end
 
   def self.entire_user_plays(username, page=1)
@@ -71,28 +71,13 @@ class BggApi
       xml["play"].each do |play|
         @players = []
         unless play["players"] == nil
-          build_players(play)
+          @hash.build_players(play, @players)
         end
-        build_plays(play, @players)
+        @hash.build_plays(play, @players, @plays)
       end
     end
     return @plays
   end
-
-  def self.build_players(play)
-    play["players"][0]["player"].each do |player|
-      @players << Hash[:name, player["name"], :win, player["win"].to_i, :score, player["score"]]
-    end
-  end
-
-  def self.build_plays(play, players)
-    if players.empty?
-      @plays << Hash[:date, play["date"], :nowinstats, play["nowinstats"].to_i, :boardgame, play["item"][0]["name"], :objectid, play["item"][0]["objectid"].to_i, :comments, play["comments"]]
-    else
-      @plays << Hash[:date, play["date"], :nowinstats, play["nowinstats"].to_i, :boardgame, play["item"][0]["name"], :objectid, play["item"][0]["objectid"].to_i, :players, players, :comments, play["comments"]]
-    end
-  end
-
 
     protected
   
