209d08a1ce327c295143da0ec76691a1615116b4
Eric Strathmeyer <eric@vawks.com>
2014-03-13 22:24:31 -0700

    Refactors BggApi spec

--- spec/bgg_api_spec.rb
+++ spec/bgg_api_spec.rb
@@ -1,60 +1,82 @@
 require 'spec_helper'
 
-describe 'BGG Search' do
-  it 'some results come back' do
-    stub_request(:any, 'http://www.boardgamegeek.com/xmlapi2/search').with(:query => {:query => 'Burgund', :type => 'boardgame'}).to_return(:body => File.new('sample_data/search?query=Burgund&type=boardgame'), :status => 200)
+describe BggApi do
+  let(:bgg) { BggApi.new }
 
-    bgg = BggApi.new
-    results = bgg.search({:query => 'Burgund', :type => 'boardgame'})
+  context 'with stubbed responses' do
 
-    results.should_not be_nil
-  end
-end
+    let(:expected_response) { File.open(response_file) }
 
-describe 'BGG Thing' do
-  it 'searches for and returns a thing' do
-    stub_request(:any, 'http://www.boardgamegeek.com/xmlapi2/thing').with(:query => {:id => '84876', :type => 'boardgame'}).to_return(:body => File.new('sample_data/thing?id=84876&type=boardgame'), :status => 200)
+    before do
+      stub_request(:any, request_url)
+        .with(:query => query)
+        .to_return(:body => expected_response, :status => 200)
+    end
 
-    bgg = BggApi.new
-    results = bgg.thing({:id => '84876', :type => 'boardgame'})
+    describe 'BGG Search' do
+      let(:query) { {:query => 'Burgund', :type => 'boardgame'} }
+      let(:request_url) { 'http://www.boardgamegeek.com/xmlapi2/search' }
+      let(:response_file) { 'sample_data/search?query=Burgund&type=boardgame' }
 
-    results.should_not be_nil
-    results['item'][0]['id'].should == '84876'
-  end
-end
+      subject(:results) { bgg.search(query) }
 
-describe 'BGG Collection' do
-  it 'retrieves a collection' do
-    stub_request(:any, 'http://www.boardgamegeek.com/xmlapi2/collection').with(:query => {:own => '1', :username => 'texasjdl', :type => 'boardgame'}).to_return(:body => File.new('sample_data/collection?username=texasjdl&own=1&excludesubtype=boardgameexpansion'), :status => 200)
+      it { should_not be_nil }
+    end
 
-    bgg = BggApi.new
-    results = bgg.collection({:username => 'texasjdl', :own => '1', :type => 'boardgame'})
+    describe 'BGG Thing' do
+      let(:query) { {:id => '84876', :type => 'boardgame'} }
+      let(:request_url) { 'http://www.boardgamegeek.com/xmlapi2/thing' }
+      let(:response_file) { 'sample_data/thing?id=84876&type=boardgame' }
 
-    results.should_not be_nil
-    results['item'][0]['objectid'].should == '421'
-  end
-end
+      subject(:results) { bgg.thing(query) }
 
-describe 'BGG Hot Items' do
-  it 'retrieves the current hot boardgames' do
-    stub_request(:any, 'http://www.boardgamegeek.com/xmlapi2/hot').with(:query => {:type => 'boardgame'}).to_return(:body => File.new('sample_data/hot?type=boardgame'), :status => 200)
+      it { should_not be_nil }
 
-    bgg = BggApi.new
-    results = bgg.hot({:type => 'boardgame'})
+      it 'retrieves the correct id' do
+        results['item'][0]['id'].should == '84876'
+      end
+    end
 
-    results.should_not be_nil
-    results['item'][0]['rank'].should == '1'
-  end
-end
+    describe 'BGG Collection' do
+      let(:query) { {:own => '1', :username => 'texasjdl', :type => 'boardgame'} }
+      let(:request_url) { 'http://www.boardgamegeek.com/xmlapi2/collection' }
+      let(:response_file) { 'sample_data/collection?username=texasjdl&own=1&excludesubtype=boardgameexpansion' }
 
-describe 'BGG Plays' do
-  it 'retrieves the plays for a user' do
-    stub_request(:any, 'http://www.boardgamegeek.com/xmlapi2/plays').with(:query => {:id => '84876', :username => 'texasjdl'}).to_return(:body => File.new('sample_data/plays?username=texasjdl&id=84876'), :status => 200)
+      subject(:results) { bgg.collection(query) }
 
-    bgg = BggApi.new
-    results = bgg.plays({:id => '84876', :username => 'texasjdl'})
+      it { should_not be_nil }
 
-    results.should_not be_nil
-    results['total'].should == '27'
+      it 'retrieves the correct id' do
+        results['item'][0]['objectid'].should == '421'
+      end
+    end
+
+    describe 'BGG Hot Items' do
+      let(:query) { {:type => 'boardgame'} }
+      let(:request_url) { 'http://www.boardgamegeek.com/xmlapi2/hot' }
+      let(:response_file) { 'sample_data/hot?type=boardgame' }
+
+      subject(:results) { bgg.hot(query) }
+
+      it { should_not be_nil }
+
+      it 'retrieves the correct rank' do
+        results['item'][0]['rank'].should == '1'
+      end
+    end
+
+    describe 'BGG Plays' do
+      let(:query) { {:id => '84876', :username => 'texasjdl'} }
+      let(:request_url) { 'http://www.boardgamegeek.com/xmlapi2/plays' }
+      let(:response_file) { 'sample_data/plays?username=texasjdl&id=84876' }
+
+      subject(:results) { bgg.plays(query) }
+
+      it { should_not be_nil }
+
+      it 'retrieves the correct total' do
+        results['total'].should == '27'
+      end
+    end
   end
 end
