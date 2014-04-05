8c1ded3620c126682fe6abcc5c57f5204c73598b
Ryan MacGillivray <rmacg89@gmail.com>
2014-04-05 13:53:59 +0100

    use hashbuilder class to handle results

--- lib/bgg-api.rb
+++ lib/bgg-api.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'xmlsimple'
+require 'hash_builder'
 
 # http://boardgamegeek.com/wiki/page/BGG_XML_API2#
 
@@ -32,11 +33,7 @@ class BggApi
     return if xml['total'] == '0'
 
     xml['item'].map do |item|
-      {
-        id:   item['id'].to_i,
-        name: item['name'][0]['value'],
-        type: item['type'],
-      }
+      HashBuilder.build_basic_game_results(item)
     end
   end
 
@@ -47,27 +44,7 @@ class BggApi
     xml = XmlSimple.xml_in(response.body)
     return if xml['boardgame'][0].has_key?('error')
 
-    game_data  = xml['boardgame'][0]
-
-    primary_name = (game_data['name'].find{ |name| name['primary'] == 'true' })['content']
-
-    other_game_names = (game_data['name'].reject { |name| name['primary'] == 'true' }).
-                         map { |node| node['content'] }
-
-    return {
-      id:    id,
-      name:  primary_name,
-
-      age:            game_data['age'][0],
-      alternatenames: other_game_names.sort,
-      description:    game_data['description'][0],
-      image:          game_data['image'][0],
-      maxplayers:     game_data['maxplayers'][0],
-      minplayers:     game_data['minplayers'][0],
-      playingtime:    game_data['playingtime'][0],
-      thumbnail:      game_data['thumbnail'][0],
-      yearpublished:  game_data['yearpublished'][0],
-    }
+    HashBuilder.build_boardgame_by_id(xml, id)
 
   end
 
@@ -92,15 +69,9 @@ class BggApi
       xml['play'].each do |play|
         players = Array.new
         unless play['players'] == nil
-          play['players'][0]['player'].each do |player|
-            players << Hash[:name, player['name'], :win, player['win'].to_i, :score, player['score']]
-          end
+          HashBuilder.build_players(play, players)
         end
-        if players.empty?
-          plays << Hash[:date, play['date'], :nowinstats, play['nowinstats'].to_i, :boardgame, play['item'][0]['name'], :objectid, play['item'][0]['objectid'].to_i, :comments, play['comments']]
-        else
-          plays << Hash[:date, play['date'], :nowinstats, play['nowinstats'].to_i, :boardgame, play['item'][0]['name'], :objectid, play['item'][0]['objectid'].to_i, :players, players, :comments, play['comments']]
-        end
+        HashBuilder.build_plays(play, players, plays)
       end
     end
     return plays
