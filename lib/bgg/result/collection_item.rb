56ff5106316c06ad976b377a82e8a7a1b7a0a2ca
Kevin Craine <kcraine@silvercloudinc.com>
2014-06-23 23:42:21 -0400

    convert collection item to new format

--- lib/bgg/result/collection_item.rb
+++ lib/bgg/result/collection_item.rb
@@ -1,69 +1,131 @@
 module Bgg
-  class Collection
-    class Item
-      attr_reader :collection_id, :comment, :id, :image, :name,
-                  :play_count, :thumbnail, :type, :year_published
+  module Result
+    class Collection
+      class Item < Item
 
-      def initialize(item)
-        # Integers
-        @id = item['objectid'].to_i
-        @collection_id = item['collid'].to_i
-        @play_count = item['numplays'][0].to_i
+        attr_reader :average_rating, :bgg_rating, :collection_id, :comment,
+                    :id, :image, :last_modified, :name, :own_count, :play_count,
+                    :play_time, :players, :thumbnail, :theme_ranks, :type,
+                    :type_rank, :user_rating, :year_published
 
-        @comment = item.fetch('comment', [])[0]
-        @image = item['image'][0]
-        @name = item['name'][0]['content']
-        @thumbnail = item['thumbnail'][0]
-        @type = item['subtype']
+        def initialize(item, request)
+          super item, request
+          set_attributes
+        end
 
-        # booleans
-        @owned = item['status'][0]['own'] == '1'
-        @for_trade = item['status'][0]['fortrade'] == '1'
-        @preordered = item['status'][0]['preordered'] == '1'
-        @want_to_buy = item['status'][0]['wanttobuy'] == '1'
-        @want_to_play = item['status'][0]['wanttoplay'] == '1'
-        @wanted = item['status'][0]['want'] == '1'
+        def for_trade?
+          @for_trade
+        end
 
-        # special handling
-        @year_published = item.fetch('yearpublished', ['0'])[0].to_i
-      end
+        def owned?
+          @owned
+        end
 
-      def played?
-        @play_count > 0
-      end
+        def played?
+          @play_count > 0 unless @play_count.nil?
+        end
 
-      def wanted?
-        @wanted
-      end
+        def preordered?
+          @preordered
+        end
 
-      def for_trade?
-        @for_trade
-      end
+        def published?
+          unless @year_published.nil?
+            @year_published != 0 and @year_published <= Time.now.year
+          end
+        end
 
-      def owned?
-        @owned
-      end
+        def wanted?
+          @wanted
+        end
 
-      def want_to_play?
-        @want_to_play
-      end
+        def want_to_buy?
+          @want_to_buy
+        end
 
-      def want_to_buy?
-        @want_to_buy
-      end
+        def want_to_play?
+          @want_to_play
+        end
 
-      def preordered?
-        @preordered
-      end
+        def game
+          #TODO refactor once Things have been coverted
+          Bgg::Game.find_by_id(self.id)
+        end
 
-      def published?
-        @year_published != 0
-      end
+        private
 
-      def game
-        Bgg::Game.find_by_id(self.id)
+        def set_attributes
+          set_default_attributes
+          set_brief_attributes if !(request.params.has_key? :brief) || request.params[:brief] == 0
+          set_stat_attributes if request.params[:stats] && request.params[:stats] == 1
+        end
+
+        def set_default_attributes
+          # Booleans
+          @for_trade = xpath_value_boolean 'status/@fortrade'
+          @owned = xpath_value_boolean 'status/@own'
+          @preordered = xpath_value_boolean 'status/@preordered'
+          @want_to_buy = xpath_value_boolean 'status/@wanttobuy'
+          @want_to_play = xpath_value_boolean 'status/@wanttoplay'
+          @wanted = xpath_value_boolean 'status/@want'
+
+          # Dates
+          @last_modified = xpath_value_time 'status/@lastmodified'
+
+          # Integers
+          @collection_id = xpath_value_int '@collid'
+          @id = xpath_value_int '@objectid'
+          @name = xpath_value 'name'
+          @type = xpath_value '@subtype'
+        end
+
+        def set_brief_attributes
+          # Integers
+          @play_count = xpath_value_int 'numplays'
+          @year_published = xpath_value_int 'yearpublished'
+
+          # Strings
+          @comment = xpath_value 'comment'
+          @image = xpath_value 'image'
+          @thumbnail = xpath_value 'thumbnail'
+        end
+
+        def set_stat_attributes
+          # Floats
+          @average_rating = xpath_value_float 'stats/rating/average/@value'
+          @bgg_rating = xpath_value_float 'stats/rating/bayesaverage/@value'
+          @user_rating = xpath_value_float 'stats/rating/@value'
+
+          # Hashes
+          @theme_ranks = xpath_value_ranks 'stats/rating/ranks/rank[@type="family"]'
+
+          # Integers
+          @own_count = xpath_value_int 'stats/@numowned'
+          @play_time = xpath_value_int 'stats/@playingtime'
+          @type_rank = xpath_value_int 'stats/rating/ranks/rank[@type="subtype"]/@value'
+
+          # Range
+          @players = xpath_value_range 'stats/@minplayers', 'stats/@maxplayers'
+        end
+
+        def xpath_value_range(start_path, end_path)
+          min_players = xpath_value_int start_path
+          max_players = xpath_value_int end_path
+          (min_players and max_players) ?  min_players..max_players : nil
+        end
+
+        def xpath_value_time(path)
+          time_string = xpath_value path
+          Time.new(time_string) if time_string
+        end
+
+        def xpath_value_ranks(path)
+          selected_ranks = @xml.xpath path
+          selected_ranks.map do |rank|
+            self.class::Rank.new rank, @request
+          end unless selected_ranks.empty?
+        end
       end
     end
   end
 end
-
--- spec/result/collection_item_spec.rb
+++ spec/result/collection_item_spec.rb
@@ -1,259 +1,227 @@
-# encoding: UTF-8
 require 'spec_helper'
 
-describe Bgg::Collection::Item do
-  let(:item_data) { {'objecttype'=>'thing',
-                     'objectid'=>'70512',
-                     'subtype'=>'boardgame',
-                     'collid'=>'11455824',
-                     'name'=>[{'sortindex'=>'1', 'content'=>'Luna'}],
-                     'yearpublished'=>['2010'],
-                     'image'=>['http://cf.geekdo-images.com/images/pic802342.jpg'],
-                     'thumbnail'=>['http://cf.geekdo-images.com/images/pic802342_t.jpg'],
-                     'status'=>[{'own'=>'1', 'prevowned'=>'0', 'fortrade'=>'0', 'want'=>'0', 'wanttoplay'=>'0', 'wanttobuy'=>'0', "wishlist"=>'0', 'preordered'=>'0', 'lastmodified'=>'2007-02-18 21:20:51'}], 'numplays'=>['7'], 'comment'=>['Never played.']} }
+describe Bgg::Result::Collection::Item do
+  let(:item_xml) { Nokogiri.XML(xml_string) }
+  let(:xml_string) { '<items><item/></items>' }
+  let(:request) { double('Bgg::Request::Collection') }
 
-  let(:item) { Bgg::Collection::Item.new(item_data) }
+  subject { Bgg::Result::Collection::Item.new(item_xml.at_xpath('items/item'), request) }
 
-  describe '.id' do
-    it 'exists' do
-      expect( item ).to respond_to(:id)
+  before do
+    request.stub(:params).and_return( {} )
+  end
+
+  context 'without data' do
+    its(:average_rating) { should eq nil }
+    its(:bgg_rating)     { should eq nil }
+    its(:collection_id)  { should eq nil }
+    its(:comment)        { should eq nil }
+    its(:for_trade?)     { should eq nil }
+    its(:id)             { should eq nil }
+    its(:image)          { should eq nil }
+    its(:name)           { should eq nil }
+    its(:last_modified)  { should eq nil }
+    its(:own_count)      { should eq nil }
+    its(:owned?)         { should eq nil }
+    its(:play_count)     { should eq nil }
+    its(:play_time)      { should eq nil }
+    its(:played?)        { should eq nil }
+    its(:players)        { should eq nil }
+    its(:preordered?)    { should eq nil }
+    its(:published?)     { should eq nil }
+    its(:theme_ranks)    { should eq nil }
+    its(:thumbnail)      { should eq nil }
+    its(:type)           { should eq nil }
+    its(:type_rank)      { should eq nil }
+    its(:user_rating)    { should eq nil }
+    its(:want_to_buy?)   { should eq nil }
+    its(:want_to_play?)  { should eq nil }
+    its(:wanted?)        { should eq nil }
+    its(:year_published) { should eq nil }
+  end
+
+  context 'with data' do
+    let(:average_rating) { 7.63834 }
+    let(:bgg_rating) { 7.34567 }
+    let(:collection_id) { 5678 }
+    let(:comment) { 'p' }
+    let(:id) { 1234 }
+    let(:image) { 'hijk' }
+    let(:last_modified) { Time.new('2001-01-01') }
+    let(:max_players) { 4 }
+    let(:min_players) { 2 }
+    let(:name) { 'defg' }
+    let(:own_count) { 123456 }
+    let(:play_count) { 2 }
+    let(:play_time) { 120 }
+    let(:status) { 1 }
+    let(:theme_rank_id) { 789 }
+    let(:thumbnail) { 'lmno' }
+    let(:type) { 'abc' }
+    let(:type_rank) { 987 }
+    let(:user_rating) { 9.5 }
+    let(:year_published) { 2014 }
+    let(:xml_string) { "
+      <items>
+        <item objectid='#{id}' subtype='#{type}' collid='#{collection_id}'>
+          <name>#{name}</name>
+          <yearpublished>#{year_published}</yearpublished>
+          <image>#{image}</image>
+          <thumbnail>#{thumbnail}</thumbnail>
+          <stats minplayers='#{min_players}'
+                 maxplayers='#{max_players}'
+                 playingtime='#{play_time}'
+                 numowned='#{own_count}'>
+            <rating value='#{user_rating}'>
+              <average value='#{average_rating}'/>
+              <bayesaverage value='#{bgg_rating}'/>
+              <ranks>
+                <rank type='subtype' value='#{type_rank}'/>
+                <rank type='family' id='#{theme_rank_id}'/>
+                <rank type='family'/>
+              </ranks>
+            </rating>
+          </stats>
+          <status own='#{status}'
+                  prevowned='#{status}'
+                  fortrade='#{status}'
+                  want='#{status}'
+                  wanttoplay='#{status}'
+                  wanttobuy='#{status}'
+                  wishlist='#{status}'
+                  preordered='#{status}'
+                  lastmodified='#{last_modified}'/>
+          <numplays>#{play_count}</numplays>
+          <comment>#{comment}</comment>
+        </item>
+      </items>" }
+
+    context 'default fields that always appear' do
+      before do
+        request.stub(:params).and_return( { brief: 1, stats: 0 } )
+      end
+
+      its(:collection_id)  { should eq collection_id }
+      its(:id)             { should eq id }
+      its(:last_modified)  { should eq last_modified }
+      its(:name)           { should eq name }
+      its(:type)           { should eq type }
+
+      context 'booleans true' do
+        let(:status)         { 1 }
+
+        its(:for_trade?)     { should eq true }
+        its(:owned?)         { should eq true }
+        its(:preordered?)    { should eq true }
+        its(:want_to_buy?)   { should eq true }
+        its(:want_to_play?)  { should eq true }
+        its(:wanted?)        { should eq true }
+      end
+
+      context 'booleans false' do
+        let(:status)     { 0 }
+
+        its(:for_trade?)     { should eq false }
+        its(:owned?)         { should eq false }
+        its(:preordered?)    { should eq false }
+        its(:want_to_buy?)   { should eq false }
+        its(:want_to_play?)  { should eq false }
+        its(:wanted?)        { should eq false }
+      end
     end
 
-    it 'returns correct value' do
-      expect( item.id ).to eq(70512)
+    context 'brief true in request params' do
+      before do
+        request.stub(:params).and_return( { brief: 1 } )
+      end
+
+      its(:comment)        { should eq nil }
+      its(:image)          { should eq nil }
+      its(:play_count)     { should eq nil }
+      its(:played?)        { should eq nil }
+      its(:published?)     { should eq nil }
+      its(:thumbnail)      { should eq nil }
+      its(:year_published) { should eq nil }
+    end
+
+    context 'brief false in request params' do
+      before do
+        request.stub(:params).and_return( { brief: 0 } )
+      end
+
+      its(:comment)        { should eq comment }
+      its(:image)          { should eq image }
+      its(:play_count)     { should eq play_count }
+      its(:thumbnail)      { should eq thumbnail }
+      its(:year_published) { should eq year_published }
+
+      context 'booleans true' do
+        let(:play_count)     { 1 }
+        let(:year_published) { 2014 }
+
+        its(:played?)        { should eq true }
+        its(:published?)     { should eq true }
+      end
+
+      context 'booleans false' do
+        let(:play_count) { 0 }
+        let(:year_published) { 0 }
+
+        its(:played?)        { should eq false }
+        its(:published?)     { should eq false }
+      end
+    end
+
+    context 'stats true in request params' do
+      before do
+        request.stub(:params).and_return( { stats: 1 } )
+      end
+
+      its(:average_rating) { should eq average_rating }
+      its(:bgg_rating)     { should eq bgg_rating }
+      its(:own_count)      { should eq own_count }
+      its(:play_time)      { should eq play_time }
+      its(:players)        { should eq min_players..max_players }
+      its(:type_rank)      { should eq type_rank }
+      its(:user_rating)    { should eq user_rating }
+
+      it do
+        expect( subject.theme_ranks.count ).to eq 2
+        expect( subject.theme_ranks.first ).to be_a Bgg::Result::Collection::Item::Rank
+        expect( subject.theme_ranks.first.id ).to eq theme_rank_id
+      end
+    end
+
+    context 'stats false in request params' do
+      before do
+        request.stub(:params).and_return(  { stats: 0 } )
+      end
+
+      its(:average_rating) { should eq nil }
+      its(:bgg_rating)     { should eq nil }
+      its(:own_count)      { should eq nil }
+      its(:play_time)      { should eq nil }
+      its(:players)        { should eq nil }
+      its(:theme_ranks)    { should eq nil }
+      its(:type_rank)      { should eq nil }
+      its(:user_rating)    { should eq nil }
+    end
+
+    describe '#game' do
+      #TODO refactor once Things have been coverted
+      let(:response_file) { 'sample_data/thing?id=70512&type=boardgame' }
+      let(:request_url) { 'http://www.boardgamegeek.com/xmlapi2/thing' }
+
+      before do
+        stub_request(:any, request_url).
+          with(query: {id: 1234, type: 'boardgame'}).
+          to_return(body: File.open(response_file), status: 200)
+      end
+
+      it 'returns a Bgg::Game object corresponding to the entry' do
+        expect( subject.game ).to be_instance_of(Bgg::Game)
+      end
     end
   end
 
-  describe '.name' do
-    it 'exists' do
-      expect( item ).to respond_to(:name)
-    end
-
-    it 'returns the correct value' do
-      expect( item.name ).to eq('Luna')
-    end
-  end
-
-  describe '.collection_id' do
-    it 'exists' do
-      expect( item ).to respond_to(:collection_id)
-    end
-
-    it 'returns the correct value' do
-      expect( item.collection_id ).to eq(11455824)
-    end
-  end
-
-  describe '.owned?' do
-    it 'exists' do
-      expect( item ).to respond_to(:owned?)
-    end
-
-    it 'returns true when own == 1' do
-      item_data['status'][0]['own'] = '1'
-      expect( item.owned? ).to eq(true)
-    end
-
-    it 'returns true when want == 0' do
-      item_data['status'][0]['own'] = '0'
-      expect( item.owned? ).to eq(false)
-    end
-  end
-
-  describe '.wanted?' do
-    it 'exists' do
-      expect( item ).to respond_to(:wanted?)
-    end
-
-    it 'returns true when want == 0' do
-      item_data['status'][0]['want'] = '0'
-      expect( item.wanted? ).to eq(false)
-    end
-
-    it 'returns true when want == 1' do
-      item_data['status'][0]['want'] = '1'
-      expect( item.wanted? ).to eq(true)
-    end
-  end
-
-  describe '.for_trade?' do
-    it 'exists' do
-      expect( item ).to respond_to(:for_trade?)
-    end
-
-    it 'returns false when fortrade == 0' do
-      item_data['status'][0]['fortrade'] = '0'
-      expect( item.for_trade? ).to eq(false)
-    end
-
-    it 'returns true when fortrade == 1' do
-      item_data['status'][0]['fortrade'] = '1'
-      expect( item.for_trade? ).to eq(true)
-    end
-  end
-
-  describe '.played?' do
-    it 'exists' do
-      expect( item ).to respond_to(:played?)
-    end
-
-    it 'returns true when play_count > 0' do
-      item_data['numplays'][0] = '7'
-      expect( item.played? ).to eq(true)
-    end
-
-    it 'returns false when play_count == 0' do
-      item_data['numplays'][0] = '0'
-      expect( item.played? ).to eq(false)
-    end
-  end
-
-  describe '.want_to_buy?' do
-    it 'exists' do
-      expect( item ).to respond_to(:want_to_buy?)
-    end
-
-    it 'returns true when wanttobuy == 1' do
-      item_data['status'][0]['wanttobuy'] = '1'
-      expect( item.want_to_buy? ).to eq(true)
-    end
-
-    it 'returns false when wanttobuy == 0' do
-      item_data['status'][0]['wanttobuy'] = '0'
-      expect( item.want_to_buy? ).to eq(false)
-    end
-  end
-
-  describe '.preordered?' do
-    it 'exists' do
-      expect( item ).to respond_to(:preordered?)
-    end
-
-    it 'returns true when preordered == 1' do
-      item_data['status'][0]['preordered'] = '1'
-      expect( item.preordered? ).to eq(true)
-    end
-
-    it 'returns false when preordered == 0' do
-      item_data['status'][0]['preordered'] = '0'
-      expect( item.preordered? ).to eq(false)
-    end
-  end
-
-  describe '.want_to_play?' do
-    it 'exists' do
-      expect( item ).to respond_to(:want_to_play?)
-    end
-
-    it 'returns true when wanttoplay == 1' do
-      item_data['status'][0]['wanttoplay'] = '1'
-      expect( item.want_to_play? ).to eq(true)
-    end
-
-    it 'returns false when wanttoplay == 0' do
-      item_data['status'][0]['wanttoplay'] = '0'
-      expect( item.want_to_play? ).to eq(false)
-    end
-  end
-
-  describe '.published?' do
-    it 'exists' do
-      expect( item ).to respond_to(:published?)
-    end
-
-    it 'returns true when yearpublished exists' do
-      expect( item.published? ).to eq(true)
-    end
-
-    it 'returns false when yearpublished does not exist' do
-      item_data.delete('yearpublished')
-      expect( item.published? ).to eq(false)
-    end
-  end
-
-  describe '.type' do
-    it 'exists' do
-      expect( item ).to respond_to(:type)
-    end
-
-    it 'returns boardgame when it is a boardgame' do
-      expect( item.type ).to eq('boardgame')
-    end
-  end
-
-  describe '.play_count' do
-    it 'exists' do
-      expect( item ).to respond_to(:play_count)
-    end
-
-    it 'returns the correct value' do
-      expect( item.play_count ).to eq(7)
-    end
-  end
-
-  describe '.year_published' do
-    it 'exists' do
-      expect( item ).to respond_to(:year_published)
-    end
-
-    it 'returns the correct value' do
-      expect( item.year_published ).to eq(2010)
-    end
-
-    it 'returns 0 if it is not published' do
-      item_data.delete('yearpublished')
-      expect( item.year_published ).to eq(0)
-    end
-  end
-
-  describe '.thumbnail' do
-    it 'exists' do
-      expect( item ).to respond_to(:thumbnail)
-    end
-
-    it 'returns the correct value' do
-      expect( item.thumbnail ).to eq('http://cf.geekdo-images.com/images/pic802342_t.jpg')
-    end
-  end
-
-  describe '.image' do
-    it 'exists' do
-      expect( item ).to respond_to(:image)
-    end
-
-    it 'returns the correct value' do
-      expect( item.image ).to eq('http://cf.geekdo-images.com/images/pic802342.jpg')
-    end
-  end
-
-  describe '.comment' do
-    it 'exists' do
-      expect( item ).to respond_to(:comment)
-    end
-
-    it 'returns the correct value' do
-      expect( item.comment ).to eq('Never played.')
-    end
-  end
-
-  describe '.game' do
-    it 'exists' do
-      expect( item ).to respond_to(:game)
-    end
-
-    it 'returns a Bgg::Game object corresponding to the entry' do
-      response_file = 'sample_data/thing?id=70512&type=boardgame'
-      request_url = 'http://www.boardgamegeek.com/xmlapi2/thing'
-
-      stub_request(:any, request_url).
-        with(query: {id: 70512, type: 'boardgame'}).
-        to_return(body: File.open(response_file), status: 200)
-
-      game = item.game
-
-      expect( game ).to be_instance_of(Bgg::Game)
-      expect( game.name ).to eq('Luna')
-      expect( game.designers ).to eq(['Stefan Feld'])
-    end
-  end
 end
