647df7a153543b99a9851f8d462f18794548d3b4
Kevin Craine <kcraine@silvercloudinc.com>
2014-08-11 17:10:10 -0400

    Add guild request object

--- lib/bgg_api.rb
+++ lib/bgg_api.rb
@@ -50,6 +50,7 @@ end
 
 require 'bgg/request/base'
 require 'bgg/request/collection'
+require 'bgg/request/guild'
 require 'bgg/request/hot'
 require 'bgg/request/plays'
 require 'bgg/request/search'
